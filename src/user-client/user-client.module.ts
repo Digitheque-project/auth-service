8c3d7967e02dee79521f7549958d7a3599815dea
sedra-tsihoarana <sedratsihoaranaherinirina@gmail.com>
2026-06-01 22:42:50 +0300

    fix: secure a

--- src/user-client/user-client.module.ts
+++ src/user-client/user-client.module.ts
@@ -11,6 +11,9 @@ import { UserClientService } from './user-client.service';
       useFactory: (configService: ConfigService) => ({
         baseURL: configService.get<string>('USER_SERVICE_URL') ?? 'http://localhost:3001',
         timeout: 5000,
+        headers: {
+          'x-api-key': configService.get<string>('INTERNAL_API_KEY') ?? '',
+        },
       }),
     }),
   ],
