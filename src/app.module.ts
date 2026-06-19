b05cc0f771841de13ee08947c80679a2bb83b409
Valisoath <nomenjanaharyvalisoathierry@gmail.com>
2026-06-19 22:18:44 +0300

    fix: migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,17 +24,12 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-
         autoLoadEntities: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
         entities: [Role, Permission],
-
-        // === FIX SSL (important) ===
-        ssl: configService.get('DB_SSL') === 'true'
-          ? { rejectUnauthorized: false }
-          : false,
-
-        synchronize: process.env.NODE_ENV !== 'production', // false en production
-        migrationsRun: true,
+        synchronize: true,
       }),
     }),
     RoleModule,
@@ -49,3 +44,59 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
   ],
 })
 export class AppModule { }
+
+
+
+
+
+// import { Module } from '@nestjs/common';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { RoleModule } from './role/role.module';
+// import { PermissionModule } from './permission/permission.module';
+// import { AuthModule } from './auth/auth.module';
+// import { Permission } from './permission/entities/permission.entity';
+// import { Role } from './role/entities/role.entity';
+// import { APP_GUARD } from '@nestjs/core';
+// import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'postgres',
+//         host: configService.get<string>('DB_HOST'),
+//         port: configService.get<number>('DB_PORT'),
+//         username: configService.get<string>('DB_USERNAME'),
+//         password: configService.get<string>('DB_PASSWORD'),
+//         database: configService.get<string>('DB_NAME'),
+
+//         autoLoadEntities: true,
+//         entities: [Role, Permission],
+
+//         // === FIX SSL (important) ===
+//         ssl: configService.get('DB_SSL') === 'true'
+//           ? { rejectUnauthorized: false }
+//           : false,
+
+//         synchronize: process.env.NODE_ENV !== 'production', // false en production
+//         migrationsRun: true,
+//       }),
+//     }),
+//     RoleModule,
+//     PermissionModule,
+//     AuthModule,
+//   ],
+//   providers: [
+//     {
+//       provide: APP_GUARD,
+//       useClass: JwtAuthGuard,
+//     },
+//   ],
+// })
+// export class AppModule { }
